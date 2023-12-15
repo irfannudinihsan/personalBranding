38e312de808e01f7b5db6e6fdecd2ecfb45cd8ea
Irfannudin Ihsan <irfannudinihsan56@gmail.com>
2023-12-15 15:47:22 +0700

    change in index.php

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php get_header(); ?>
 
-<div class="container mx-auto my-8">
+<div class="container mx-auto my-8 bg-slate-950 color-blue ">
 
-	<?php if ( have_posts() ) : ?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
 
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+<!--  -->
+	<div class="row">
+	<div class="col">
+	<p>Hi, I'm</p>
+	<H1>Irfannudin Ihsan</H1>
 
-		<?php endwhile; ?>
 
-	<?php endif; ?>
+
+	<h4>Web Developer</h4>
+
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+	</div>
+
+	<div class="col">
+
+	<p>image</p>
+	<img src="" alt="">
+
+	</div>
+
+	</div>
+
+	
 
 </div>
 
