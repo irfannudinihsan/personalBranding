386624ef14a4413bbff627b7059eaa2d91bdb251
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-12-18 06:32:51 +0700

    content

--- index.php
+++ index.php
@@ -262,6 +262,351 @@
 
 
 
+<!-- section Achivement -->
+
+<section  class="pt-40">
+
+<div class="container">
+
+
+<!-- title section -->
+<div class="flex flex-wrap center">
+
+<!-- description section -->
+<div class="w-full lg:w-1/2  ">
+	<h1 class="mb-3">
+		Marking Significant Achivement
+	</h1>
+
+	<p class="mb-3">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, ipsam a! Doloremque.
+	</p>
+
+	<ul class="mb-3">
+		<li class="flex"><span class="mr-3"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
+<g fill="#ea5819" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M41.9375,8.625c-0.66406,0.02344 -1.27344,0.375 -1.625,0.9375l-18.8125,28.78125l-12.1875,-10.53125c-0.52344,-0.54297 -1.30859,-0.74609 -2.03125,-0.51953c-0.71875,0.22266 -1.25391,0.83203 -1.37891,1.57422c-0.125,0.74609 0.17578,1.49609 0.78516,1.94531l13.9375,12.0625c0.4375,0.37109 1.01563,0.53516 1.58203,0.45313c0.57031,-0.08594 1.07422,-0.41016 1.38672,-0.89062l20.09375,-30.6875c0.42969,-0.62891 0.46484,-1.44141 0.09375,-2.10547c-0.37109,-0.66016 -1.08594,-1.05469 -1.84375,-1.01953z"></path></g></g>
+</svg>
+ </span>Lorem ipsum dolor sit amet.</li>
+		<li class="flex"><span class="mr-3"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
+<g fill="#ea5819" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M41.9375,8.625c-0.66406,0.02344 -1.27344,0.375 -1.625,0.9375l-18.8125,28.78125l-12.1875,-10.53125c-0.52344,-0.54297 -1.30859,-0.74609 -2.03125,-0.51953c-0.71875,0.22266 -1.25391,0.83203 -1.37891,1.57422c-0.125,0.74609 0.17578,1.49609 0.78516,1.94531l13.9375,12.0625c0.4375,0.37109 1.01563,0.53516 1.58203,0.45313c0.57031,-0.08594 1.07422,-0.41016 1.38672,-0.89062l20.09375,-30.6875c0.42969,-0.62891 0.46484,-1.44141 0.09375,-2.10547c-0.37109,-0.66016 -1.08594,-1.05469 -1.84375,-1.01953z"></path></g></g>
+</svg>
+ </span>Lorem ipsum dolor sit amet.</li>
+		<li class="flex"><span class="mr-3"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
+<g fill="#ea5819" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M41.9375,8.625c-0.66406,0.02344 -1.27344,0.375 -1.625,0.9375l-18.8125,28.78125l-12.1875,-10.53125c-0.52344,-0.54297 -1.30859,-0.74609 -2.03125,-0.51953c-0.71875,0.22266 -1.25391,0.83203 -1.37891,1.57422c-0.125,0.74609 0.17578,1.49609 0.78516,1.94531l13.9375,12.0625c0.4375,0.37109 1.01563,0.53516 1.58203,0.45313c0.57031,-0.08594 1.07422,-0.41016 1.38672,-0.89062l20.09375,-30.6875c0.42969,-0.62891 0.46484,-1.44141 0.09375,-2.10547c-0.37109,-0.66016 -1.08594,-1.05469 -1.84375,-1.01953z"></path></g></g>
+</svg>
+ </span>Lorem ipsum dolor sit amet.</li>
+	</ul>
+
+	<div class="mb-3">
+	<a href="" class="inline-block bg-orange-600 py-2 px-4 rounded-lg text-white mb-6  text-md">Consultation</a>
+	</div>
+
+	
+
+
+</div>
+
+<!-- content section -->
+<div class="w-full lg:w-1/2 ">
+
+
+<div class="grid grid-col-1 md:grid-cols-1 lg:grid-cols-2 gap-16">
+
+<!-- item grid -->
+<div class="flex flex-col items-center justify-center rounded-xl shadow-lg aspect-video bg-orange-100 relative">
+
+<div class="absolute -top-8">
+
+<div class="p-2 bg-orange-600 rounded-full mx-auto inline-block mb-2">
+	<svg role="img"  width="60" height="60" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+</div>
+
+<h1>1K</h1>
+<p>Happy Customers</p>
+</div>
+<!-- item grid -->
+<div class="flex flex-col items-center justify-center rounded-xl shadow-lg aspect-video bg-orange-100 relative">
+
+<div class="absolute -top-8">
+
+<div class="p-2 bg-orange-600 rounded-full mx-auto inline-block mb-2">
+	<svg role="img"  width="60" height="60" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+</div>
+
+<h1>1K</h1>
+<p>Happy Customers</p>
+</div>
+<!-- item grid -->
+<div class="flex flex-col items-center justify-center rounded-xl shadow-lg aspect-video bg-orange-100 relative">
+
+<div class="absolute -top-8">
+
+<div class="p-2 bg-orange-600 rounded-full mx-auto inline-block mb-2">
+	<svg role="img"  width="60" height="60" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+</div>
+
+<h1>1K</h1>
+<p>Happy Customers</p>
+</div>
+<!-- item grid -->
+<div class="flex flex-col items-center justify-center rounded-xl shadow-lg aspect-video bg-orange-100 relative">
+
+<div class="absolute -top-8">
+
+<div class="p-2 bg-orange-600 rounded-full mx-auto inline-block mb-2">
+	<svg role="img"  width="60" height="60" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+</div>
+
+<h1>1K</h1>
+<p>Happy Customers</p>
+</div>
+
+</div>
+
+
+
+</div>
+	
+
+
+
+</div>
+
+</div>
+
+</section>
+
+
+<section class="pt-40">
+
+<div class="container">
+
+<!-- title section -->
+<div class="">
+
+<p>Customers Review</p>
+<h1>What Client</h1>
+</div>
+
+<div class="flex flex-wrap center justify-between">
+
+<!-- card customer -->
+<div class="w-full lg:w-1/2 mx-auto px-2  ">
+
+<div class="p-6 rounded-xl border-orange-600  border-2  mb-6">
+
+<div class="flex mb-4">
+	
+		<img class="mx-1" width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+	
+
+</div>
+
+<p class=" mb-4">
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quisquam qui! Placeat voluptas consequuntur explicabo?
+</p>
+
+<div class=" mb-4">
+
+<div class="flex">
+<img width="60px" class="rounded-full"  src="http://latihan1.local/wp-content/themes/personalBranding/images/photo.png" alt="irfannudin ihsan" >
+			
+
+<div class="mx-1">
+<h1 class="text-lg font-bold ">Jonn Canedy </h1>
+<p class="text-sm font-bold text-slate-500">Web Developer</p>
+
+</div>
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+<!-- card customer -->
+<div class="w-full lg:w-1/2 mx-auto px-2  ">
+
+<div class="p-6 rounded-xl border-orange-600  border-2  mb-6">
+
+<div class="flex mb-4">
+	
+		<img class="mx-1" width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+		<img class="mx-1"  width="24px" height="24px" src="http://latihan1.local/wp-content/themes/personalBranding/images/star.png" alt="">
+	
+
+</div>
+
+<p class=" mb-4">
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, quisquam qui! Placeat voluptas consequuntur explicabo?
+</p>
+
+<div class=" mb-4">
+
+<div class="flex">
+<img width="60px" class="rounded-full"  src="http://latihan1.local/wp-content/themes/personalBranding/images/photo.png" alt="irfannudin ihsan" >
+			
+
+<div class="mx-1">
+<h1 class="text-lg font-bold ">Jonn Canedy </h1>
+<p class="text-sm font-bold text-slate-500">Web Developer</p>
+
+</div>
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+</div>
+
+</section>
+<section class="pt-40">
+
+<div class="container">
+
+<!-- title section -->
+<div class="">
+
+<p>SUBCRIBE</p>
+<h1>CONTACT US </h1>
+</div>
+
+<div class="flex flex-wrap center justify-between">
+
+
+<!-- card customer -->
+<div class="w-full lg:w-1/2    ">
+
+<div class="relative mt-10 -z-1 ">
+				<img src="http://latihan1.local/wp-content/themes/personalBranding/images/photo.png" alt="irfannudin ihsan" class="max-w-full mx-auto">
+			</div>
+
+
+
+</div>
+
+<!-- card customer -->
+<div class="w-full lg:w-1/2 mx-auto  ">
+
+<form action="">
+
+<div class="mb-4">
+
+<label
+          for=""
+          form="email"
+          class="block font-poppins font-semibold mb-1  dark:text-slate-100"
+          >Your Name</label
+        >
+        <input
+          type="email"
+          id="email"
+          placeholder="masukan email..."
+          class="border w-full px-2 py-2 block rounded-lg placeholder:text-slate-500 " />
+
+</div>
+<div class="mb-4">
+
+<label
+          for=""
+          form="email"
+          class="block font-poppins font-semibold mb-1  dark:text-slate-100"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          placeholder="masukan email..."
+          class="border w-full px-2 py-2 block rounded-lg placeholder:text-slate-500 " />
+
+</div>
+<div class="mb-4">
+
+<label
+          for=""
+          form="email"
+          class="block font-poppins font-semibold mb-1  dark:text-slate-100"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          placeholder="masukan email..."
+          class="border w-full px-2 py-2 block rounded-lg placeholder:text-slate-500 " />
+
+</div>
+<div class="mb-4">
+
+<label
+          for=""
+          form="email"
+          class="block font-poppins font-semibold mb-1  dark:text-slate-100"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          placeholder="masukan email..."
+          class="border w-full px-2 py-2 block rounded-lg placeholder:text-slate-500 " />
+
+</div>
+
+<div class="mb-4">
+<a href="" class="inline-block bg-orange-600 py-3 px-8 rounded-full text-white mb-6  text-md">Download CV</a>
+	</div>
+        
+      </form>
+
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+</div>
+
+</section>
+
+
+
 
 <?php
 get_footer();
