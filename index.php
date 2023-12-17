b2d7aa0b8597a6c9c35107af1b065d615c5cb7cb
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-12-17 18:17:37 +0700

    section 2

--- index.php
+++ index.php
@@ -4,28 +4,26 @@
 
 <!-- section hero -->
 <section id="home" class="pt-40 ">
-
-<div class="container bg-orange-900">
-	<div class="flex flex-wrap">
+<div class="container">
+	<div class="flex flex-wrap center">
 		<!-- description hero -->
 		<div class="w-full px-4 lg:w-1/2">
-
 		<p class="font-bold text-xl text-orange-600">Hi, Im</p>
 		<h1 class="font-bold text-4xl lg:text-6xl text-blue-800 mb-4" >Irfannudin Ihsan</h1>
 		<p class="font-bold text-xl text-blue-800 mb-4" >Web Developer</p>
 		<p class="text-slate-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel tempora, perferendis cum provident iste necessitatibus!</p>
 
-
 		<div class="mt-10 ">
-		<a href="" class="bg-orange-600 py-3 px-8 rounded-full text-white ">Download CV</a>
-		<a href="" class="bg-white py-3 px-8 rounded-full text-orange-600 border border-2 border-orange-600 ">Contact</a>
+		<a href="" class="inline-block bg-orange-600 py-3 px-8 rounded-full text-white mb-6  text-md">Download CV</a>
+		<a href="" class="inline-block bg-white py-3 px-8 rounded-full text-orange-600 border border-2 border-orange-600 text-md">Contact</a>
 		</div>
 
 
 		</div>
 
+		<!-- description image -->
 		<div class="w-full px-4 lg:w-1/2">
-			<div class="relative mt-10">
+			<div class="relative mt-10 -z-1 ">
 				<img src="http://latihan1.local/wp-content/themes/personalBranding/images/photo.png" alt="irfannudin ihsan" class="max-w-full mx-auto">
 			<span class="absolute bottom-0 -z-10  left-1/2 -translate-x-1/2" >
 				<img class="lg:w-full"  src="http://latihan1.local/wp-content/themes/personalBranding/images/blob.png" >
@@ -35,22 +33,92 @@
 		</div>
 
 	</div>
+</section>
 
-	<div class="mx-auto w-lg h-4 bg-slate-800 text-pink-800">
 
-	<h1 class="text-pink-800">halo</h1>
-	<h1>halo</h1>
-	<h1>halo</h1>
-	<h1>halo</h1>
-	<h1>halo</h1>
-	<h1>halo</h1>
-	<h1>halo</h1>
+<!-- counting -->
+<section>
+<div  class="mx-auto lg:w-[1000px] bg-white p-8 rounded-full -mt-8 z-10 absolute left-1/2 transform -translate-x-1/2 shadow-xl flex justify-evenly">
 
-	</div>
+<div class="flex">
+<div class="p-2 bg-orange-600 rounded-full mx-1 ">
+
+	<svg role="img"  width="28" height="28" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+<div class="mx-1">
+<h1 class="text-lg font-bold text-blue-800">8 Years Job </h1>
+<p class="text-sm font-bold text-orange-600">Experience</p>
+
+</div>
+
+</div>
+<div class="flex">
+<div class="p-2 bg-orange-600 rounded-full mx-1 ">
+
+	<svg role="img"  width="28" height="28" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+<div class="mx-1">
+<h1 class="text-lg font-bold text-blue-800">8 Years Job </h1>
+<p class="text-sm font-bold text-orange-600">Experience</p>
+
+</div>
+
+</div>
+<div class="flex">
+<div class="p-2 bg-orange-600 rounded-full mx-1 ">
+
+	<svg role="img"  width="28" height="28" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>WordPress</title><path d="M21.469 6.825c.84 1.537 1.318 3.3 1.318 5.175 0 3.979-2.156 7.456-5.363 9.325l3.295-9.527c.615-1.54.82-2.771.82-3.864 0-.405-.026-.78-.07-1.11m-7.981.105c.647-.03 1.232-.105 1.232-.105.582-.075.514-.93-.067-.899 0 0-1.755.135-2.88.135-1.064 0-2.85-.15-2.85-.15-.585-.03-.661.855-.075.885 0 0 .54.061 1.125.09l1.68 4.605-2.37 7.08L5.354 6.9c.649-.03 1.234-.1 1.234-.1.585-.075.516-.93-.065-.896 0 0-1.746.138-2.874.138-.2 0-.438-.008-.69-.015C4.911 3.15 8.235 1.215 12 1.215c2.809 0 5.365 1.072 7.286 2.833-.046-.003-.091-.009-.141-.009-1.06 0-1.812.923-1.812 1.914 0 .89.513 1.643 1.06 2.531.411.72.89 1.643.89 2.977 0 .915-.354 1.994-.821 3.479l-1.075 3.585-3.9-11.61.001.014zM12 22.784c-1.059 0-2.081-.153-3.048-.437l3.237-9.406 3.315 9.087c.024.053.05.101.078.149-1.12.393-2.325.609-3.582.609M1.211 12c0-1.564.336-3.05.935-4.39L7.29 21.709C3.694 19.96 1.212 16.271 1.211 12M12 0C5.385 0 0 5.385 0 12s5.385 12 12 12 12-5.385 12-12S18.615 0 12 0"/></svg>
+</div>
+
+<div class="mx-1">
+<h1 class="text-lg font-bold text-blue-800">8 Years Job </h1>
+<p class="text-sm font-bold text-orange-600">Experience</p>
+
+</div>
+
+</div>
 
 </div>
 
 </section>
+
+
+
+
+<!-- section service -->
+<section id="service" class="pt-40 -z-100  ">
+
+<div class="container">
+
+<div class="w-full flex flex-col md:flex-row">
+
+<div class="lg:w-1/3 ">
+		<h2 class="text-4xl text-blue-800 font-bold" >Exploring My Design Expertise</h2>
+	</div>
+	<div class="lg:w-2/3 ">
+		<p class="text-slate-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni beatae nesciunt, aliquam eius fugit corrupti!</p>
+	</div>
+</div>
+
+	
+	
+
+	<div>
+
+
+	</div>
+	
+
+
+
+
+</div>
+
+</section>
+
+
 
 
 <?php
