c1d87d3f23684f81cad9b991a9af0882f3164084
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-12-24 14:43:19 +0700

    detail article

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,12 +1,12 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article class="bg-white-700  shadow-2xl p-8 mt-40 lg:mx-48 xl:mx-80 2xl:mx-[400px] shadow-slate-200 rounded-xl" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header mb-4">
-	<?php the_post_thumbnail('medium') ?>
-		<?php the_title( sprintf( '<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
+	<?php the_post_thumbnail('medium rounded-lg mb-4  object-cover') ?>
+		<?php the_title( sprintf( '<h1 class="entry-title text-lg md:text-3xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<time class="text-sm text-slate-500" datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
 	</header>
 
-	<div class="entry-content">
+	<div class="entry-content text-slate-600 text-sm">
 
 		<?php the_content(); ?>
 
