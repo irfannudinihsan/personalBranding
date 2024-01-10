3493b5ae4e84430ee52306ef74196aab6e0e181f
irfannudin ihsan <irfannudinihsan56@gmail.com>
2024-01-10 08:20:10 +0700

    cek

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 
 		<?php do_action('tailpress_header'); ?>
 
-		<header class=" absolute left-0 w-full z-50 py-6 font-poppins bg-white ">
+		<header class=" absolute left-0 w-full z-50 py-8 font-poppins bg-white dark:bg-slate-800">
 
 			<div class="mx-auto container  ">
 				<div class="lg:flex mx-auto lg:justify-between lg:items-center  ">
@@ -71,9 +71,9 @@
 
 
 
-<input data-hs-theme-switch class="relative w-[3.25rem] mx-2 h-7 bg-yellow-400 checked:bg-none checked:bg-blue-600 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 border border-transparent ring-1 ring-transparent focus:border-slate-700 focus:ring-slate-700 focus:outline-none appearance-none
+<input data-hs-theme-switch class="relative w-[3.25rem] mx-2 h-7 bg-slate-50 checked:bg-none checked:bg-slate-100 border-2 border-yellow-400 checked:border-blue-500 rounded-full cursor-pointer transition-colors ease-in-out duration-200 border border-transparent ring-1 ring-transparent focus:border-slate-700 focus:ring-slate-700 focus:outline-none appearance-none
 
-before:inline-block before:w-6 before:h-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200
+before:inline-block before:w-6 before:h-6 before:bg-yellow-400 checked:before:bg-blue-500 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200
 
 after:absolute after:end-1.5 after:top-[calc(50%-0.40625rem)] after:w-[.8125rem] after:h-[.8125rem] after:bg-no-repeat after:bg-[right_center] after:bg-[length:.8125em_.8125em]  after:transform after:transition-all after:ease-in-out after:duration-200 after:opacity-70 checked:after:start-1.5 checked:after:end-auto" type="checkbox" id="darkSwitch">
 
--- js/app.js
+++ js/app.js
@@ -82,14 +82,14 @@ window.addEventListener('load', function () {
     });
   });
 });
-var darkToggle = document.querySelector('#dark-toggle');
-var html = document.querySelector('html');
-darkToggle.addEventListener('click', function () {
-  if (darkToggle.checked) {
-    html.classList.add('dark');
-  } else {
-    html.classList.remove('dark');
-  }
+
+// Navigation toggle
+window.addEventListener('load', function () {
+  var main_navigation = document.querySelector('#primary-menu');
+  document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
+    e.preventDefault();
+    main_navigation.classList.toggle('hidden');
+  });
 });
 
 /***/ }),
