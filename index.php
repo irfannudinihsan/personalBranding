7574218c04a93c0dbb846ebf4edd60db78f46c7b
irfannudin ihsan <irfannudinihsan56@gmail.com>
2024-02-08 14:09:50 +0700

    post limit 3

--- index.php
+++ index.php
@@ -291,18 +291,20 @@
 
 		<div class=" grid grid-cols-1 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-3 gap-3 ">
 
-			<!-- test index copy -->
-			<?php if (have_posts()) : ?>
-				<?php
-				while (have_posts()) :
+			
+			<?php
+			$count = 0; 
+
+			if (have_posts()) :
+				while (have_posts() && $count < 3) : 
 					the_post();
-				?>
-
+					$count++; 
+			?>
 					<?php get_template_part('template-parts/content', get_post_format()); ?>
-
-				<?php endwhile; ?>
-
-			<?php endif; ?>
+			<?php
+				endwhile;
+			endif;
+			?>
 
 		</div>
 
